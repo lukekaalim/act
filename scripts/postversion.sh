872df9f28345754a842ee3181ebfeb6c58304af0
Luke Kaalim <lukekaalim@gmail.com>
2021-10-11 04:24:55 +1100

    Update postversion script

--- scripts/postversion.sh
+++ scripts/postversion.sh
@@ -1,10 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S bash -e
 
 name=$(cat package.json | jq .name -r)
 version=$(cat package.json | jq .version -r)
 tag="$name@$version"
 
-git add .
+echo $(cd ..; npm i)
+
+git add . ../package-lock.json
 git commit -m $tag
 git tag -a $tag -m "Publied version $version of $name"
 
