0886a411377041ad49955e0e8208f1a80c3c83cb
Luke Kaalim <lukekaalim@gmail.com>
2021-11-08 11:19:37 +1100

    Create three/scene & three/webglrenderer

---
+++ renderers/three/components/object.js
@@ -0,0 +1,27 @@
+// @flow strict
+/*:: import type {
+  Vector3,
+  Object3D,
+  Quaternion,
+  BufferGeometry,
+  Material,
+  Texture,
+  Color,
+  Group,
+  Camera,
+  PerspectiveCamera,
+  Euler
+} from "three"; */
+
+/*::
+export type Object3DProps<T> = {
+  ref?: ((reference: T) => mixed) | { current: T | any },
+
+  name?: string,
+  position?: Vector3,
+  rotation?: Euler,
+  quaternion?: Quaternion,
+  visible?: boolean,
+  scale?: Vector3,
+};
+*/
