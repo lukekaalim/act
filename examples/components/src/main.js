3a60fb1ec5902e8dad802f01ab484a084a34a380
Luke Kaalim <lukekaalim@gmail.com>
2022-09-13 19:49:51 +1000

    Add luke flow-typed gitmodule

--- examples/components/src/main.js
+++ examples/components/src/main.js
@@ -1,7 +1,6 @@
 // @flow strict
 import { h, useRef, useState } from '@lukekaalim/act';
 import { render } from '@lukekaalim/act-web';
-import { useCurve, useCurves } from '@lukekaalim/act-curve';
 import { toWords } from 'number-to-words';
 
 const App = () => {
@@ -11,18 +10,7 @@ const App = () => {
   const [duration, setDuration] = useState(1000);
   const [t, toggle] = useState(false);
 
-  useCurve(v, x => ref.current && (
-    ref.current.style.transform = `translate(${x}px)`,
-    ref.current.textContent = toWords(x),
-    ref.current.style.backgroundColor = `hsl(${Math.floor(x) % 360}, 70%, 80%)`
-  ), { duration });
-
   const v2 = t ? { x: 0, y: 0 } : { x: 150, y: 150 };
-
-  useCurves(v2, v2 => ref2.current && (
-    ref2.current.textContent = JSON.stringify({ x: Math.floor(v2.x), y: Math.floor(v2.y) }),
-    ref2.current.style.transform = `translate(${v2.x}px, ${v2.y}px)`
-  ));
 
   return [
     h('p', { ref, style: { padding: 20, display: 'inline-block', position: 'relative' } }),
