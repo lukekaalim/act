ea95f4f29aba61b83ccc82fb28e4de0f02fca6d2
Luke Kaalim <lukekaalim@gmail.com>
2023-03-08 02:53:58 +1100

    Create act-alias with basic react implementation

---
+++ libraries/alias/react.js
@@ -0,0 +1,22 @@
+// @flow strict
+import {
+  createElement,
+  
+  useEffect,
+  useState,
+  useContext,
+
+  useMemo,
+  
+  createContext,
+} from 'react';
+
+export {
+  createElement,
+  createElement as h,
+  createContext,
+  useMemo,
+  useContext,
+  useState,
+  useEffect,
+}
