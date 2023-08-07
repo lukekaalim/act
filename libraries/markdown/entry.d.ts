dbdf03c279401858bca6a0f9f4a43c5d5acdf5c3
Luke Kaalim <lukekaalim@gmail.com>
2023-08-07 12:58:06 +1000

    Add typescript declaration to act-markdown

---
+++ libraries/markdown/entry.d.ts
@@ -0,0 +1,32 @@
+
+import { Component } from '@lukekaalim/act';
+import type { Root, Content } from 'mdast'
+
+export function parseMarkdown(text: string): Root;
+
+export type ComponentMap = {
+  [nodeType: string]: Component<{ node: Content }>
+};
+
+export type MarkdownRendererProps = {
+  markdownText: string,
+  directiveComponents?: ComponentMap,
+  externalComponents?: ComponentMap,
+};
+
+export const MarkdownRenderer: Component<MarkdownRendererProps>;
+
+export type MarkdownASTRendererProps = {
+  root: MarkdownASTNode,
+  directiveComponents?: ComponentMap,
+  externalComponents?: ComponentMap,
+};
+
+export const MarkdownASTRenderer: Component<MarkdownASTRendererProps>;
+
+export type MarkdownNodeProps = {
+  node: Content,
+  spread?: boolean
+}
+
+export const MarkdownNode: Component<MarkdownNodeProps>;
