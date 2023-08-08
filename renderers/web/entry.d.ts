9f341f1ed744d6812276b5da5dd654024d93e6a2
Luke Kaalim <lukekaalim@gmail.com>
2023-08-09 00:30:22 +1000

    Export setNodeChildren2 in typescript

--- renderers/web/entry.d.ts
+++ renderers/web/entry.d.ts
@@ -4,3 +4,4 @@ export const render: (actElement: Element, htmlElement: HTMLElement) => void
 
 export * from './renderer';
 export * from './props';
+export * from './node';
---
+++ renderers/web/node.d.ts
@@ -0,0 +1,1 @@
+declare export function setNodeChildren2(parent: Node, children: Node[]): void
