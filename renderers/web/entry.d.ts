9aaffa049ad9f79a9b65150a26fb4d87a780e629
Luke Kaalim <lukekaalim@gmail.com>
2023-08-09 00:28:43 +1000

    Export setWebProps2 in typescript for props

--- renderers/web/entry.d.ts
+++ renderers/web/entry.d.ts
@@ -3,3 +3,4 @@ import { Element } from "@lukekaalim/act";
 export const render: (actElement: Element, htmlElement: HTMLElement) => void
 
 export * from './renderer';
+export * from './props';
---
+++ renderers/web/props.d.ts
@@ -0,0 +1,7 @@
+import { Diff3, DiffSet } from "@lukekaalim/act-reconciler";
+
+declare export function setWebProps2(
+  element: Node,
+  set: DiffSet,
+  diff: Diff3,
+): void;
