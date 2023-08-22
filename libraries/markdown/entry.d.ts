8eec3e4c9ad3db027f13903ca7beda3bfea5f284
Luke Kaalim <lukekaalim@gmail.com>
2023-08-23 00:42:55 +1000

    Update types to be more specific for act markdown typescript

--- libraries/markdown/entry.d.ts
+++ libraries/markdown/entry.d.ts
@@ -1,31 +1,37 @@
 
-import { Component } from '@lukekaalim/act';
-import type { Root, Content } from 'mdast'
+import { Component, Props } from '@lukekaalim/act';
+import type { Root, RootContent, RootContentMap } from 'mdast'
+import type { Directives } from "mdast-util-directive";
+
 
 export function parseMarkdown(text: string): Root;
 
-export type ComponentMap = {
-  [nodeType: string]: Component<{ node: Content }>
+export type MarkdownDirectiveComponentProps = {
+  node: Directives,
+}
+
+export type ComponentMap<TProps extends Props> = {
+  [nodeType: string]: Component<TProps>
 };
 
 export type MarkdownRendererProps = {
   markdownText: string,
-  directiveComponents?: ComponentMap,
-  externalComponents?: ComponentMap,
+  directiveComponents?: ComponentMap<MarkdownDirectiveComponentProps>,
+  externalComponents?: ComponentMap<{ node: RootContentMap[keyof RootContentMap] }>,
 };
 
 export const MarkdownRenderer: Component<MarkdownRendererProps>;
 
 export type MarkdownASTRendererProps = {
   root: Root,
-  directiveComponents?: ComponentMap,
-  externalComponents?: ComponentMap,
+  directiveComponents?: ComponentMap<MarkdownDirectiveComponentProps>,
+  externalComponents?: ComponentMap<{ node: RootContentMap[keyof RootContentMap] }>,
 };
 
 export const MarkdownASTRenderer: Component<MarkdownASTRendererProps>;
 
 export type MarkdownNodeProps = {
-  node: Content,
+  node: RootContentMap[keyof RootContentMap],
   spread?: boolean
 }
 
